b9ab83dc0a39e60c3dbb1aaa896ad2ef3a756465
William Owens <owensjrw@gmail.com>
2022-07-31 09:53:59 -0500

    Delete java/hamming directory

--- java/hamming/src/main/java/Hamming.java
+++
@@ -1,9 +0,0 @@
-public class Hamming {
-    public Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-
-    public int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
-    }
-}
