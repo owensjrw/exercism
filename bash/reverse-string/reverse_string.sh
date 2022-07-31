4cf7c5738d9478f4f6c08dab681ae85c89326124
William Owens <owensjrw@gmail.com>
2022-07-31 09:53:14 -0500

    Delete bash directory

--- bash/reverse-string/reverse_string.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-a_string={$1}
-
-# set length of string
-
-length=${#a_string}
-
-echo "Lenght of string is $length"
--- bash/two-fer/two_fer.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-[[ $# -eq 0 ]] && you_var=you || you_var=$1
-
-echo "One for $you_var, one for me."
