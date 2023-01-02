b6a97b4d612923e725fe24e6531892ec06bcc29e
William Owens <owensjrw@gmail.com>
2023-01-02 17:23:10 -0600

    Update linked_list.h

--- linked-list/linked_list.h
+++ linked-list/linked_list.h
@@ -8,17 +8,6 @@
 #define FOREACH(L, F, N, C) struct list_node *_node = NULL;\
   struct list_node *C = NULL;\
   for(C = _node = L->F; _node != NULL; C = _node = _node->N)
-/*
-#define clean_errno() (errno == 0 ? "None" : strerror(errno))
-
-#define log_err(M, ...) fprintf(stderr,\
-  "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__,\
-  clean_errno(), ##__VA_ARGS__)
-#define  check(A, M, ...) if(!(A)) {\
-  log_err(M, ##__VA_ARGS__); errno = 0; goto error;}
-
-#define check_mem(A) check((A), "Out of memory.")*/
-//End Shaw Code.
 
 typedef int ll_data_t;
 struct list;
