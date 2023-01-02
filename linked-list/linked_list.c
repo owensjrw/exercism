6afb71fbf3d893da77add70d05ab200eb324ee7e
William Owens <owensjrw@gmail.com>
2023-01-02 13:47:42 -0600

    Update linked_list.c

--- linked-list/linked_list.c
+++ linked-list/linked_list.c
@@ -1,11 +1,8 @@
 #include "linked_list.h"
+#include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <stddef.h>
-
-#define FOREACH(L, F, N, C) struct list_node *_node = NULL;\
-                            struct list_node *C = NULL;\
-  for(C = _node = L->F; _node != NULL; C = _node = _node->N)
 
 struct list_node {
   struct list_node *prev, *next;
@@ -19,11 +16,7 @@ struct list {
 
 // constructs a new (empty) list
 struct list *list_create(void) {
-  struct list *list = malloc(sizeof(struct list));
-  assert(list);
-  list->first = list->last = NULL;
-  list->count = 0;
-  return list;
+  return calloc(1, sizeof(struct list));
 }
 
 // counts the items on a list
@@ -33,97 +26,83 @@ size_t list_count(const struct list *list){
 
 // inserts item at back of a list
 void list_push(struct list *list, ll_data_t item_data) {
-  struct list_node *node = malloc(sizeof(struct list_node));
+  struct list_node *node = calloc(1, sizeof(struct list_node));
+  //check_mem(node);
   assert(node);
   node->data = item_data;
-  list->count++;
 
-  if (list->first == NULL) {
-      list->first = node;
-      node->prev = node->next = NULL;
-    } else if (list->last == NULL) {
-      node->prev = list->first;
-      list->last = node;
+  if (list->count == 0) {
+      node->next = NULL;    node->prev = NULL;
+      list->first = node;   list->last = node;
     } else {
-      node->prev = list->last;
-      list->last = node;
+      node->prev = list->last;  node->next = NULL;
+      list->first->next = node; list->last = node;
     }
+
+  list->count += 1;
 }
 
 // removes item from back of a list
 ll_data_t list_pop(struct list *list) {
+  if (list->count == 0) {
+      fprintf(stderr, "List is empty.");
+      return 0;
+    }
   struct list_node *node = list->last;
   ll_data_t popped = node->data;
-  list->count--;
-
-  if (list->last->prev == list->first) {
-      list->last = NULL;
+  if (list->count == 1) {
+      list->first = NULL;   list->last = NULL;
     } else {
       list->last = node->prev;
       list->last->next = NULL;
     }
-
-  free(node->prev);
-  free(node->next);
+  list->count -= 1;
   free(node);
   return popped;
 }
 
 // inserts item at front of a list
 void list_unshift(struct list *list, ll_data_t item_data) {
-  struct list_node *node = malloc(sizeof(struct list_node));
+  struct list_node *node = calloc(1, sizeof(struct list_node));
+  //check_mem(node);
   assert(node);
   node->data = item_data;
-  list->count++;
 
-  if (list->first == NULL) {
-      list->first = node;
-      node->prev = node->next = NULL;
+  if (list->count == 0) {
+      node->next = NULL;    node->prev = NULL;
+      list->first = node;   list->last = node;
     } else {
-      node->next = list->first;
-      node->prev = NULL;
-      list->first->prev = node;
-      list->first = node;
+      node->next = list->first;   node->prev = NULL;
+      list->last->prev = node;   list->first = node;
     }
+
+  list->count += 1;
 }
 
 // removes item from front of a list
 ll_data_t list_shift(struct list *list) {
+  if (list->count == 0) {
+      fprintf(stderr, "List is empty.");
+      return 0;
+    }
   struct list_node *node = list->first;
-  ll_data_t shift = node->data;
-  list->count--;
-
-  if (list->first->next == NULL) {
-      list->first = NULL;
+  ll_data_t popped = node->data;
+  if (list->count == 1) {
+      list->first = NULL;   list->last = NULL;
     } else {
       list->first = node->next;
       list->first->prev = NULL;
     }
-
-  free(node->prev);
-  free(node->next);
+  list->count -= 1;
   free(node);
-  return shift;
+  return popped;
 }
 
 // deletes a node that holds the matching data
 void list_delete(struct list *list, ll_data_t data) {
   FOREACH(list, first, next, node) {
     if (node->data == data) {
-      node->prev->next = node->next;
-      node->next->prev = node->prev;
-      if (list->first == node) {
-          list->first = node->next;
-        } else if (list->last == node) {
-          list->last = node->prev;
-        }
-      if (list->first == list->last) {
-          list->last = NULL;
-        }
       list->count--;
-      free(node->next);
-      free(node->prev);
-      free(node);
       break;
     }
   }
